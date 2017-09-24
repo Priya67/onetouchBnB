acd76187047274681fb4fb32e9bbdd7e45eba5d0
Priya <mangalpriya67@gmail.com>
2017-09-24 11:51:37 -0700

    Corrected errors

--- frontend/components/SpotMap/spot_map.jsx
+++ frontend/components/SpotMap/spot_map.jsx
@@ -1,46 +1,46 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { withRouter } from 'react-router-dom';
-//
-// import MarkerManager from '../../util/marker_manager';
-//
-// const getCoordsObj = latlng => ({
-//   lat: latlng.lat(),
-//   lng: latlng.lng()
-// });
-//
-// const mapOptions = {
-//   center: {
-//     lat: 37.773972,
-//     lng: -122.431297
-//   },
-//   zoom: 123
-// };
-//
-// class SpotMap extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//
-//   componentDidMount() {
-//     const map = this.refs.map;
-//     this.map = new google.maps.Map(map, mapOptions);
-//     this.MarkerManager = new MarkerManager(this.map, this.handleMarkerClick.bind(this));
-//     if(this.props.singleSpot) {
-//       this.props.fetchSpot(this.props.spotId);
-//     } else {
-//       this.registerListeners();
-//       this.MarkerManager.updateMarkers(this.props.spots);
-//     }
-//   }
-//
-//   render() {
-//     return (
-//       <div id="map-container" ref="map">
-//
-//       </div>
-//     );
-//   }
-// }
-//
-// export default SpotMap;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router-dom';
+
+import MarkerManager from '../../util/marker_manager';
+
+const getCoordsObj = latlng => ({
+  lat: latlng.lat(),
+  lng: latlng.lng()
+});
+
+const mapOptions = {
+  center: {
+    lat: 37.773972,
+    lng: -122.431297
+  },
+  zoom: 123
+};
+
+class SpotMap extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const map = this.refs.map;
+    this.map = new google.maps.Map(map, mapOptions);
+    this.MarkerManager = new MarkerManager(this.map, this.handleMarkerClick.bind(this));
+    if(this.props.singleSpot) {
+      this.props.fetchSpot(this.props.spotId);
+    } else {
+      this.registerListeners();
+      this.MarkerManager.updateMarkers(this.props.spots);
+    }
+  }
+
+  render() {
+    return (
+      <div id="map-container" ref="map">
+
+      </div>
+    );
+  }
+}
+
+export default SpotMap;
