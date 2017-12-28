f4a648555ddbc57f2be003c7980753332b7802cd
Priya <mangalpriya67@gmail.com>
2017-12-28 03:59:52 -0800

    Placed validations for phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
             :phone_number, presence: true
   validates :username, :session_token, :email, uniqueness: true
   validates :password, length: {minimum: 6}, allow_nil: true
+  validates :phone_number, numericality: true, length: { is: 10 }
 
   attr_reader :password
   after_initialize :ensure_session_token
